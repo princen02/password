54171c38427bd706037c5880bec95a494da9d8e9
Prince Ndebele <princendex460@hotmail.com>
2021-10-03 23:46:28 +0200

    We were able to make the user choose more than one option

--- script.js
+++ script.js
@@ -18,7 +18,27 @@ function generatePassword(){
   var numberCharacters = "0123456789";
   var specialCharacters = "!@#$%^&*()";
   var lengthOfPassword = parseInt(prompt("Enter the length of the password"))
-  if(lengthOfPassword < 8 || lengthOfPassword > 126){
+  if(lengthOfPassword < 8 || lengthOfPassword > 126 ||isNaN(lengthOfPassword)){
     return "Invalid password length"
+  }
+  var useUpper = confirm("Do you like to have uppercase letters in password");
+  var useLower = confirm("Do you like to have lowercase letters in password");
+  var useNumber = confirm("Do you like to have number letters in password");
+  var useSpecial = confirm("Do you like to have special character letters in password");
+  var acceptedCharacters =[]
+  if(useUpper){
+    acceptedCharacters += acceptedCharacters + upperCaseCharacters
+  }
+  if(useLower){
+    acceptedCharacters += acceptedCharacters + lowerCaseCharacters
+  }
+  if(useNumber){
+    acceptedCharacters += acceptedCharacters + numberCharacters
+  }
+  if(useSpecial){
+    acceptedCharacters += acceptedCharacters + specialCharacters
+  }
+  if(acceptedCharacters.length == 0){
+    return "Choose more than one option"
   }
 }
